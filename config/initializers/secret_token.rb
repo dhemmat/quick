725abd765391d6eaee5bb39758f49fb1399c6d72
David R. Hemmat <dhemmat@gmail.com>
2013-07-23 14:38:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickpress::Application.config.secret_token = '3c33a63a9381a764ce7090e779e0c7b0074fd54159a5e935d65b63c8448b0100a087321e43f63e580ef6eae051480190a9981eddc84a7e4a75aae18279594ec5'
